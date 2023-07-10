cbc3d9142e87560e4c4289a8597b1cd84f7536a3
turkbera <turk.mbera@gmail.com>
2023-07-11 01:55:46 +0200

    all done except sessions

--- projects/2020/x/wiki/encyclopedia/urls.py
+++ projects/2020/x/wiki/encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("newEntry", views.newPage, name= "newPage"),
     path("wiki/<str:entry>", views.entry, name="entry"),
-    path("randomPage/", views.randomPage, name="randomPage")
+    path("randomPage/", views.randomPage, name="randomPage"),
+    path("edit/<str:entry>", views.editPage, name="editPage"),
+    path("search", views.search, name="search"),
     ]
--- projects/2020/x/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/encyclopedia/views.py
@@ -11,6 +11,22 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def markDown2Html(entry):
+    html = util.get_entry(entry)
+    #headings
+    # Handle headings
+    html = re.sub(r'^(#+)\s*(.+)', r'<h1>\2</h1>', html, flags=re.MULTILINE)    
+    html = re.sub(r'^##\s*(.+)', r'<h2>\1</h2>', html, flags=re.MULTILINE)
+    html = re.sub(r'^###\s*(.+)', r'<h3>\1</h3>', html, flags=re.MULTILINE)
+    #bold
+    html = re.sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', html)
+    #unordered list
+    html = re.sub(r'^\* *(.+)', r'<ul><li>\1</li></ul>', html, flags=re.MULTILINE)
+    #link
+    html = re.sub(r'\[(.+?)\]\((.+?)\)', r'<a href="\2">\1</a>', html)
+    #paragraph
+    html = re.sub(r'^([^<\n].+)$', r'<p>\1</p>', html, flags=re.MULTILINE)
+    return html
 
 def entry(request, entry):
     if util.get_entry(entry):
@@ -25,25 +41,10 @@ def entry(request, entry):
         return render(request, "encyclopedia/newPage.html", {
             "form":NewEntryForm()
         })
-def markDown2Html(entry):
-    html = util.get_entry(entry)
-    #headings
-    html = re.sub(r'^#\s*(.+)', r'<h1>\1</h1>', html, flags=re.MULTILINE)
-    html = re.sub(r'^##\s*(.+)', r'<h2>\1</h2>', html, flags=re.MULTILINE)
-    html = re.sub(r'^###\s*(.+)', r'<h3>\1</h3>', html, flags=re.MULTILINE)
-    #bold
-    html = re.sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', html)
-    #unordered list
-    html = re.sub(r'^\* *(.+)', r'<ul><li>\1</li></ul>', html, flags=re.MULTILINE)
-    #link
-    html = re.sub(r'\[(.+?)\]\((.+?)\)', r'<a href="\2">\1</a>', html)
-    #paragraph
-    html = re.sub(r'^([^<\n].+)$', r'<p>\1</p>', html, flags=re.MULTILINE)
-    return html
 
 class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Title of New Entry")
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(label="Title of New Entry", widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={"class": "form-control"}))
 
     def is_valid(self):
         valid = super().is_valid()
@@ -68,7 +69,7 @@ def newPage(request):
                 title = form.cleaned_data['title']
                 content = form.cleaned_data['content']
                 util.save_entry(title, content)
-                return HttpResponseRedirect(reverse("encyclopedia:entry", args=[content]))
+                return HttpResponseRedirect(reverse("encyclopedia:entry", args=[title]))
         else:
             return render(request, "encyclopedia/newPage.html", {
                 "form": form
@@ -82,3 +83,41 @@ def randomPage(request):
     entries = util.list_entries()
     random_entry = random.choice(entries)
     return HttpResponseRedirect(reverse("encyclopedia:entry", args=(random_entry,)))
+
+class editEntryForm(forms.Form):
+    content = forms.CharField(label="",widget=forms.Textarea(attrs={"class":"form-control"}))
+
+    def clean_content(self):
+        content = self.cleaned_data['content'].strip()
+        return content
+
+
+
+def editPage(request, entry):
+    entry = entry.strip()
+    if request.method == "POST":
+        form = editEntryForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data['content'].strip()
+            util.save_entry(entry, content)
+            return HttpResponseRedirect(reverse("encyclopedia:entry", args=[entry]))
+    else:
+        content = util.get_entry(entry)
+        form = editEntryForm(initial={'content': content})
+    
+    return render(request, "encyclopedia/editPage.html", {
+        "form": form,
+        "entry": entry
+    })
+
+def search(request):
+    query = request.GET.get('q', '')
+    entries = util.list_entries()
+    results = [entry for entry in entries if query.lower() in entry.lower()]
+    
+    if query.lower() in [entry.lower() for entry in entries]:
+        return HttpResponseRedirect(reverse("encyclopedia:entry", args=[query]))
+    
+    return render(request, "encyclopedia/search.html", {
+        "entries": results,
+    })
